55a9e631f51853ddec799beb145fa8ac8c9f1ce5
Chip Miller <charlespmiller@gmail.com>
2013-02-05 22:26:29 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialDevotional::Application.config.secret_token = '338fb0261061ab433e84319f1e3ccd93a62c3cb56d4e28b7c83db1a6eb49aa97bd6dcde5f089c94e3d57ce50e909b1d3864e038aa603e01260db7fa797a83282'
