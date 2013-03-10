3f60da4c6ad31a66f6a954ed8f53f72372b520c8
Chip Miller <charlespmiller@gmail.com>
2013-03-09 17:58:36 -0800

    fix admin auth

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -28,7 +28,7 @@
 
 class AdminUser < ActiveRecord::Base
   # Authentication Layer
-  devise :database_authenticatable, :encryptable, :trackable, :validatable, :recoverable, :confirmable, :lockable, :timeoutable, 
+  devise :database_authenticatable, :trackable, :validatable, :recoverable, :confirmable, :lockable, :timeoutable, 
          :lock_strategy => :failed_attempts, :maximum_attempts => 5, :unlock_strategy => :email,        # :lockable
          :timeout_in => 12.hours,                                                                       # :timeoutable
          :stretches => 20                                                                               # :database_authenticatable
