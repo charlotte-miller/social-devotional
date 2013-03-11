55bcec07f0cd1ceea2fa022fee8cc7b252769b5a
Chip Miller <charlespmiller@gmail.com>
2013-03-10 20:13:56 -0700

    adding polymorphic_questions_path helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,16 @@
 module QuestionsHelper
+  
+  def polymorphic_questions_path( question_or_action=nil )
+    question = question_or_action.is_a?( Question ) ? question_or_action : 'questions'
+    obj_array = [@series, @lesson, @group, @meeting, question].compact
+    
+    case question_or_action
+      when 'new'    
+        obj_array[-1] = obj_array[-1].singularize
+        new_polymorphic_path( obj_array )
+      # not edit
+      else  polymorphic_path( obj_array )
+    end
+  end
+  
 end
--- spec/helpers/questions_helper_spec.rb
+++ spec/helpers/questions_helper_spec.rb
@@ -11,5 +11,51 @@ require 'spec_helper'
 #   end
 # end
 describe QuestionsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'polymorphic_questions_path' do
+    before(:each) do
+      @question = build_stubbed(:question, id:100)
+    end
+    
+    context "library" do
+      before(:each) do
+        assign :series, build_stubbed(:series, slug: 'matthew-study')
+        assign :lesson, build_stubbed(:lesson, id: 1)
+      end
+      
+      it "should build index/create" do
+        helper.polymorphic_questions_path.should == '/library/matthew-study/lessons/1/questions' #match %r{ /library/matthew-study/lessons/\d/questions }
+      end
+
+      it "should build new" do
+        helper.polymorphic_questions_path('new').should == '/library/matthew-study/lessons/1/questions/new'
+      end
+      
+      it "should build show" do
+        helper.polymorphic_questions_path(@question).should == '/library/matthew-study/lessons/1/questions/100' #match %r{ /library/matthew-study/lessons/\d/questions }
+      end
+      
+    end
+    
+    context "groups" do
+      before(:each) do
+        assign :group,   build_stubbed(:group,    id: 1)
+        assign :meeting, build_stubbed(:meeting,  id: 1)
+      end
+      
+      it "should build index/create" do
+        helper.polymorphic_questions_path.should == '/groups/1/meetings/1/questions' #match %r{ /library/matthew-study/lessons/\d/questions }
+      end
+
+      it "should build new" do
+        helper.polymorphic_questions_path('new').should == '/groups/1/meetings/1/questions/new'
+      end
+      
+      it "should build show" do
+        helper.polymorphic_questions_path(@question).should == '/groups/1/meetings/1/questions/100' #match %r{ /library/matthew-study/lessons/\d/questions }
+      end
+
+    end
+    
+  end
 end
