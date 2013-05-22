e93007feb765ab38412e2677bfe1a42b0832f51f
Chip Miller <charlespmiller@gmail.com>
2013-05-21 22:55:21 -0700

    WIP Paperclip/S3 configuration - Study#video_url moved into lesson as video - Lesson video and audio now paperclip - added study unit testing
    - TODO Tests Passing 
    -- Can't set slug on study [gets overwitten] :(
    - TODO test file upload through console and with external url
    - TODO split Lesson#paperclip into a Concern module

--- config/initializers/friendly_id.rb
+++ config/initializers/friendly_id.rb
@@ -2,5 +2,5 @@ FriendlyId.defaults do |config|
   config.use :slugged, :history
   config.use :reserved
     # Reserve words for English and Spanish URLs
-    config.reserved_words = %w(new edit nueva nuevo editar search)
+    config.reserved_words = %w(new edit nueva nuevo editar search library groups info studies about bible books audio video)
 end
