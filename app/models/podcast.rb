71f94df864d4a327a79a9355b50b40972da07974
Chip Miller <charlespmiller@gmail.com>
2013-06-02 10:41:55 -0700

    WIP Podcast#update_channel

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -16,7 +16,8 @@ class Podcast < ActiveRecord::Base
   # ---------------------------------------------------------------------------------
   # Attributes
   # ---------------------------------------------------------------------------------
-  attr_accessible :church_id, :last_checked, :title, :url
+  attr_accessible :title, :url
+  attr_accessible :church_id, :last_checked, :last_updated, :as => :factory_girl
   delegate :name, :homepage, :to => :church, :prefix => :church # church_name, church_homepage
 
   # ---------------------------------------------------------------------------------
@@ -24,8 +25,8 @@ class Podcast < ActiveRecord::Base
   # ---------------------------------------------------------------------------------
   belongs_to :church, :inverse_of => :podcasts
   has_many :studies,  :inverse_of => :podcast do
-    def most_recent
-      order('last_published_at ASC').last
+    def most_recent(n=nil)
+      order('last_published_at ASC').last(n)
     end
   end
 
@@ -50,6 +51,8 @@ class Podcast < ActiveRecord::Base
       podcast_arr = Array.wrap(podcasts)
       
       Podcast::Collector.new(podcast_arr) do |podcast_obj, podcast_xml|
+        podcast_obj.touch(:last_checked)
+        
         channel = Podcast::Normalize::Channel.new(podcast_xml)
         if channel.last_updated > podcast_obj.last_updated
           podcast_obj.update_channel( channel )
@@ -60,7 +63,22 @@ class Podcast < ActiveRecord::Base
   end #class << self
   
   def update_channel( normalized_channel )
-    self.studies.most_recent
-    Study.update_from_channel()
+    # Areas of Responcibility
+    # - item
+    # - podcast (self)
+    # - studies 
+    # - lesson
+    
+    recent_studies = studies.most_recent(5)
+    normalized_channel.items.each do |item|  
+      # 0) build lesson
+      # 1) skip existing
+      # 2) check if it's similar to the last lesson from the last 5 studies (last_published_at DESC)
+      # => recent_studies.map(&:last_lesson).reverse.select {|lesson| lesson.similar? new_lesson}
+      # 3) assign or create study
+      # 4) save lesson
+    end
+    
+    touch(:last_updated)
   end
 end
--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -15,7 +15,7 @@
 require 'spec_helper'
 
 describe Podcast do
-  let(:podcast) { create(:podcast) }
+  subject { create(:podcast) }
   
   it { should belong_to :church }
   it { should have_many :studies }
@@ -24,6 +24,27 @@ describe Podcast do
   
   it "builds from factory", internal:true do
     lambda { create(:podcast) }.should_not raise_error
+  end
+  
+  describe '#studies.most_recent( n=nil )' do
+    before(:each) do
+      @podcast = create(:podcast_with_n_studies, n:2 )
+      @study1, @study2 = @studies = @podcast.studies
+    end
+    
+    it "returns the most recently published lessons" do
+      @podcast.studies.most_recent.last_published_at.should be > @study1.last_published_at 
+    end
+        
+    it "returns an obj or sorted collection (depending on N)" do
+      recent_1 = @podcast.studies.most_recent
+      recent_n = @podcast.studies.most_recent(2)
+      
+      recent_1.should be_a Study
+      recent_n.should be_a Array
+      recent_n.should have(2).items
+      recent_n[0].last_published_at < recent_n[1].last_published_at
+    end
   end
   
   describe '.pull_updates( podcasts )' do
@@ -47,11 +68,17 @@ describe Podcast do
       Podcast::Collector.should_receive( :new ).with([@podcast]).and_return(@runner)
       Podcast.pull_updates(@podcast)
     end
-    
+        
     describe '- after fetching the podcast XML', internal:true do
       before(:each) do
         @podcast_xml = File.read(File.join(Rails.root, 'spec/files/podcast_xml', 'itunes.xml'))      
         stub_request(:get, %r{/podcasts/audio_podcast.xml$}).to_return( :body => @podcast_xml, :status => 200 )
+      end
+      
+      it "updates #last_checked" do
+        inital_last_checked = @podcast.last_checked
+        Timecop.travel(1.minute) { Podcast.pull_updates(@podcast) }
+        @podcast.last_checked.should be > inital_last_checked
       end
     
       it "updates each podcast" do
@@ -72,7 +99,7 @@ describe Podcast do
   end
   
   describe '#update_channel( normalized_channel )' do
-    let(:channel_obj) { Podcast::Normalized::Channel.new(File.read(File.join(Rails.root, 'spec/files/podcast_xml', 'itunes.xml'))) }
+    let(:channel_obj) { Podcast::Normalize::Channel.new(File.read(File.join(Rails.root, 'spec/files/podcast_xml', 'itunes.xml'))) }
     
     it "creates a lesson from a podcast item" do
       pending
@@ -84,6 +111,12 @@ describe Podcast do
     
     it "adds similar lessons to the same study" do
       pending
+    end
+    
+    it "updates #last_updated" do
+      inital_last_updated = subject.last_updated
+      Timecop.travel(1.minute) { subject.update_channel(channel_obj) }
+      subject.last_updated.should be > inital_last_updated
     end
   end
 
