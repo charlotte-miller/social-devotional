71986c1270c6da9a84ba9e4fe48590b885a6e2de
Chip Miller <charlespmiller@gmail.com>
2013-10-14 22:46:17 -0700

    adding s3-rrs and permissions

--- app/models/concerns/attachable_file.rb
+++ app/models/concerns/attachable_file.rb
@@ -9,15 +9,17 @@ module AttachableFile
     # 
     # *Generates:*
     #  has_attached_file :video,
-    #    :storage => :s3,
-    #    :bucket => AppConfig.s3.bucket,
-    #    :s3_credentials => AppConfig.s3.credentials,
-    #    :path => ':rails_env/:class/:attachment/:updated_at-:basename.:extension'
+    #    :storage          => :s3,
+    #    :s3_storage_class => :reduced_redundancy,
+    #    :s3_permissions   => :public_read,
+    #    :bucket           => AppConfig.s3.bucket,
+    #    :s3_credentials   => AppConfig.s3.credentials,
+    #    :path             => ':rails_env/:class/:attachment/:updated_at-:basename.:extension'
     #  
     #  attr_accessible :video_original_url
     #  attr_reader :video_remote_url
     #  def video_remote_url=(url_str)
-    #    self.video_original_url = url_str
+    #    self.video_original_url = @video_remote_url = url_str
     #    
     #    # require lib/extensions/active_record/instance_after_save
     #    def self.after_save
@@ -27,15 +29,17 @@ module AttachableFile
     #  end
     #  
     #
-    def has_attachable_file( attachment_name, options={} )
+    def has_attachable_file( attachment_name, options={} )      
       class_eval do
         has_attached_file attachment_name, {
-          :storage => :s3,
-          :bucket => AppConfig.s3.bucket,
-          :s3_credentials => AppConfig.s3.credentials,
-          # :url  => AppConfig.cloudfront.url 
+          :storage          => :s3,
+          :s3_storage_class => :reduced_redundancy,
+          :s3_permissions   => :public_read,
+          :bucket           => AppConfig.s3.bucket,
+          :s3_credentials   => AppConfig.s3.credentials,
+          # :url            => AppConfig.cloudfront.url 
           # :path => [ DEFINE IN OPTIONS ]
-        }.merge(options)
+        }.deep_merge(options)
       end
       
       # https://github.com/thoughtbot/paperclip/wiki/Attachment-downloaded-from-a-URL
@@ -46,7 +50,7 @@ module AttachableFile
         attr_accessible :#{attachment_name}_original_url
         attr_reader :#{attachment_name}_remote_url
         def #{attachment_name}_remote_url=(url_str)
-          self.#{attachment_name}_original_url = url_str
+          self.#{attachment_name}_original_url = @#{attachment_name}_remote_url = url_str
           
           # require lib/extensions/active_record/instance_after_save
           def self.after_save
