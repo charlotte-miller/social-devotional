a28f1024799c9a37e609bc0c28668c68f03d401d
Chip Miller <charlespmiller@gmail.com>
2014-02-10 17:53:29 -0800

    shorter session key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SocialDevotional::Application.config.session_store :cookie_store, key: '_social_devotional_session'
+SocialDevotional::Application.config.session_store :cookie_store, key: '_sd_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
