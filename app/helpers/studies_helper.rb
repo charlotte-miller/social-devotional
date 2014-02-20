e67f02e659d2f48350f73351634d7a199290ac64
Chip Miller <charlespmiller@gmail.com>
2014-02-20 09:55:50 -0800

    StudiesHelper grid methods refactored into new class

--- app/helpers/studies_helper.rb
+++ app/helpers/studies_helper.rb
@@ -1,28 +1,45 @@
 module StudiesHelper
-  GRID_LAYOUT_COLUMN_COUNTS = [ 
-      [3,3,3,3],
-      [4,4,4],
-      [3,4,5],
-      [3,3,6]
-    ].inject([]) {|array,grid_width| (array | grid_width.permutation.to_a.uniq)}
   
-  def random_grid_layout
-    GRID_LAYOUT_COLUMN_COUNTS.sample
+  def grid_layout_for(collection, options={})
+    GridLayout.new(collection, options).rows
   end
   
-  # Creates a visually 'random' grid 
-  def random_grid_layouts(count=10, rows_since_possible_repeat=3)
-    def find_uniq_layout
-     @trailing_n_rows.empty? ? (return random_grid_layout) : (maybe = random_grid_layout)
-     maybe = find_uniq_layout if @trailing_n_rows.include?(maybe)
-     maybe = find_uniq_layout if @trailing_n_rows.last[0] == maybe[0]
-     maybe = find_uniq_layout if @trailing_n_rows.last[-1] == maybe[-1]
-     maybe
+  class GridLayout
+    LAYOUT_COLUMN_COUNTS = [ 
+        [3,3,3,3],
+        [4,4,4],
+        [3,4,5],
+        [3,3,6]
+      ].inject([]) {|array,grid_width| (array | grid_width.permutation.to_a.uniq)}
+    
+    def initialize(collection, options={})
+      options = {
+        rows_since_possible_repeat: 3
+      }.merge(options)
+      
+      @collection = collection
+      @rows = []
     end
     
-    count.times.inject([]) do |array,index|      
-      @trailing_n_rows = array[[index-rows_since_possible_repeat, 0].max...index]
-      array << find_uniq_layout
+    def random_grid_layout
+      LAYOUT_COLUMN_COUNTS.sample
+    end
+    
+    # Creates a visually 'random' grid 
+    def rows_for(count=10, rows_since_possible_repeat=3)    
+      count.times.inject([]) do |array,index|      
+        @trailing_n_rows = array[[index-rows_since_possible_repeat, 0].max...index]
+        array << find_uniq_layout
+      end
+    end
+    
+  private
+    def find_uniq_layout
+      @trailing_n_rows.empty? ? (return random_grid_layout) : (maybe = random_grid_layout)
+      maybe = find_uniq_layout if @trailing_n_rows.include?(maybe)
+      maybe = find_uniq_layout if @trailing_n_rows.last[0] == maybe[0]
+      maybe = find_uniq_layout if @trailing_n_rows.last[-1] == maybe[-1]
+      maybe
     end
   end
 end
--- spec/helpers/studies_helper_spec.rb
+++ spec/helpers/studies_helper_spec.rb
@@ -1,35 +1,44 @@
 require 'spec_helper'
 
-describe StudiesHelper do
-  
-  describe "#random_grid_layout" do
-    it "grabs a random sample of GRID_LAYOUT_COLUMN_COUNTS" do
-      StudiesHelper::GRID_LAYOUT_COLUMN_COUNTS.should include helper.random_grid_layout
-    end
+describe StudiesHelper, :focus do
+    
+  describe "grid_layout_for(collection, options={})" do
+    
   end
   
-  describe "#random_grid_layouts(count=10, rows_since_possible_repeat=3)" do
-    before(:all){ @orig_verbose, $VERBOSE = $VERBOSE, nil }
-    after(:all){ $VERBOSE = @orig_verbose}
+  describe '::GridLayout' do
+    let(:collection) { (1..5).to_a }
+    subject { StudiesHelper::GridLayout.new(collection) }
     
-    it "returns N samples of GRID_LAYOUT_COLUMN_COUNTS" do
-      helper.random_grid_layouts(5).should have(5).items
-      helper.random_grid_layouts(5).each do |layout|
-        StudiesHelper::GRID_LAYOUT_COLUMN_COUNTS.should include layout
+    describe "#random_grid_layout" do
+      it "grabs a random sample of LAYOUT_COLUMN_COUNTS" do
+        StudiesHelper::GridLayout::LAYOUT_COLUMN_COUNTS.should include subject.random_grid_layout
       end
     end
     
-    it "does not repeat for 'rows_since_possible_repeat'" do
-      StudiesHelper::GRID_LAYOUT_COLUMN_COUNTS = [[1], [2], [3]] # length must be > rows_since_possible_repeat
-      helper.random_grid_layouts(6, 2).flatten.sort.should eq [1,1,2,2,3,3]
-    end
+    describe "#rows_for(count=10, rows_since_possible_repeat=3)" do
+      before(:all){ @orig_verbose, $VERBOSE = $VERBOSE, nil }
+      after(:all){ $VERBOSE = @orig_verbose}
     
-    it "does not allow immediately repeated values for first/last value" do
-      (layouts = helper.random_grid_layouts(50)).each_with_index do |layout, index|
-        next if index == 0
-        trailing_layout = layouts[index-1]
-        trailing_layout[0].should_not  eql layout[0]
-        trailing_layout[-1].should_not eql layout[-1]
+      it "returns N samples of LAYOUT_COLUMN_COUNTS" do
+        subject.rows_for(5).should have(5).items
+        subject.rows_for(5).each do |layout|
+          StudiesHelper::GridLayout::LAYOUT_COLUMN_COUNTS.should include layout
+        end
+      end
+    
+      it "does not repeat for 'rows_since_possible_repeat'" do
+        StudiesHelper::GridLayout::LAYOUT_COLUMN_COUNTS = [[1], [2], [3]] # length must be > rows_since_possible_repeat
+        subject.rows_for(6, 2).flatten.sort.should eq [1,1,2,2,3,3]
+      end
+    
+      it "does not allow immediately repeated values for first/last value" do
+        (layouts = subject.rows_for(50)).each_with_index do |layout, index|
+          next if index == 0
+          trailing_layout = layouts[index-1]
+          trailing_layout[0].should_not  eql layout[0]
+          trailing_layout[-1].should_not eql layout[-1]
+        end
       end
     end
   end
