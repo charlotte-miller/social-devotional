1811ebb8c65255af1bd872449d424a25623b1a0e
Chip Miller <charlespmiller@gmail.com>
2014-03-01 09:51:41 -0800

    Cleaned-up Chruch#import_site

--- app/models/church.rb
+++ app/models/church.rb
@@ -46,7 +46,7 @@ class Church < ActiveRecord::Base
   def import_site(options={})
     options = {
       start_path: 'sermons/',
-      lesson_matcher: /(sermon\/\D*$)/,
+      lesson_matcher: /(sermon\/\D*\/$)/,
       pagination_matcher: /(\/resources\/sermons\/\?page=\d*)/,
     }
     
@@ -54,16 +54,18 @@ class Church < ActiveRecord::Base
     
     Anemone.crawl(homepage + options[:start_path]) do |anemone|
       anemone.focus_crawl {|page| page.links.map{|link| (link_matcher =~ link.to_s) && link }.compact }
-      anemone.on_every_page do |page|
-        url_str = page.url.to_s
-        if (page.code == 200) && (options[:lesson_matcher] =~ page.url.to_s)
-          puts "#{page.code} - #{page.url}"
-          # Study.find_or_create_by(name:"")
-          # adapter = Lesson::Adapters::Web.new_from_url(url_str)
-          # lesson  = Lesson.new_from_adapter(adapter)
-        end
-        
+      
+      anemone.on_pages_like options[:lesson_matcher] do |page|
+        puts "#{page.code} - #{page.url}"
+        # Study.find_or_create_by(name:"")
+        # nokogiri_doc = page.doc
+        # adapter = Lesson::Adapters::Web.new_from_url(url_str)
+        # lesson  = Lesson.new_from_adapter(adapter)
       end
+      
+      # anemone.on_every_page do |page|
+      #   puts "#{page.code} - #{page.url}"
+      # end
     end
   end
 end
